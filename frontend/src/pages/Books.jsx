a251fbbf283c2aabde438ad8f68bf2705acad7e0
pepe1973 <bodrogipr@gmail.com>
2025-01-01 12:47:42 +0100

    2025.01.01. Mentés

--- frontend/src/pages/Books.jsx
+++ frontend/src/pages/Books.jsx
@@ -3,7 +3,8 @@ import Navbar from '../components/Navbar';
 
 const Books = () => {
     const [books, setBooks] = useState([]);
-
+    const [tipusok, setTipusok] = useState([]);
+    const [arak, setArak] = useState('');
     useEffect(() => {
         const dolgoz = async () => {
             try {
@@ -12,9 +13,74 @@ const Books = () => {
                 );
 
                 const valasz = await response.json();
+                let munka = valasz.books;
 
                 if (response.ok) {
-                    setBooks(valasz.books);
+                    if (arak === '' || arak === 'nincs') {
+                        if (tipusok.length === 0) {
+                            if (munka.length === 0) {
+                                window.alert(
+                                    'Nincs a feltételeknek megfelelő elem!'
+                                );
+                            } else {
+                                setBooks(munka);
+                            }
+                        } else {
+                            munka = munka.filter((elem) => {
+                                let ertek = false;
+                                for (let i = 0; i < tipusok.length; i++) {
+                                    ertek ||= elem.tipus === tipusok[i];
+                                }
+
+                                return ertek;
+                            });
+
+                            if (munka.length === 0) {
+                                window.alert(
+                                    'Nincs a feltételeknek megfelelő elem!'
+                                );
+                            } else {
+                                setBooks(munka);
+                            }
+                        }
+                    } else {
+                        if (tipusok.length === 0) {
+                            munka = munka.filter(
+                                (elem) =>
+                                    elem.ar >= +arak.split('-')[0] &&
+                                    elem.ar <= +arak.split('-')[1]
+                            );
+                            if (munka.length === 0) {
+                                window.alert(
+                                    'Nincs a feltételeknek megfelelő elem!'
+                                );
+                            } else {
+                                setBooks(munka);
+                            }
+                        } else {
+                            munka = munka.filter(
+                                (elem) =>
+                                    elem.ar >= +arak.split('-')[0] &&
+                                    elem.ar <= +arak.split('-')[1]
+                            );
+                            munka = munka.filter((elem) => {
+                                let ertek = false;
+                                for (let i = 0; i < tipusok.length; i++) {
+                                    ertek ||= elem.tipus === tipusok[i];
+                                }
+
+                                return ertek;
+                            });
+
+                            if (munka.length === 0) {
+                                window.alert(
+                                    'Nincs a feltételeknek megfelelő elem!'
+                                );
+                            } else {
+                                setBooks(munka);
+                            }
+                        }
+                    }
                 } else {
                     window.alert(valasz.msg);
                 }
@@ -24,17 +90,80 @@ const Books = () => {
         };
 
         dolgoz();
-    }, []);
+    }, [tipusok, arak]);
 
     function megmutat(id) {
         window.location.href = `/uniquebook/${id}`;
+    }
+
+    function levalogat(ertek) {
+        if (!tipusok.includes(ertek)) {
+            setTipusok([...tipusok, ertek]);
+        } else {
+            setTipusok(tipusok.filter((elem) => elem !== ertek));
+        }
     }
 
     return (
         <>
             <Navbar />
             <div className="books-kontener">
-                <div className="books-bal-kontener"></div>
+                <div className="books-bal-kontener">
+                    <div className="bal-fix">
+                        <h4>Típus</h4>
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <label htmlFor="Regény">Regény</label>
+                                    </td>
+                                    <td>
+                                        <input
+                                            type="checkbox"
+                                            id="Regény"
+                                            value="Regény"
+                                            onChange={(e) =>
+                                                levalogat(e.target.value)
+                                            }
+                                        />
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <label htmlFor="Magazin">Magazin</label>
+                                    </td>
+                                    <td>
+                                        <input
+                                            type="checkbox"
+                                            id="Magazin"
+                                            value="Magazin"
+                                            onChange={(e) =>
+                                                levalogat(e.target.value)
+                                            }
+                                        />
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <label htmlFor="Képregény">
+                                            Képregény
+                                        </label>
+                                    </td>
+                                    <td>
+                                        <input
+                                            type="checkbox"
+                                            id="Képregény"
+                                            value="Képregény"
+                                            onChange={(e) =>
+                                                levalogat(e.target.value)
+                                            }
+                                        />
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
                 <div className="books-kozep-kontener">
                     {books.map((book) => (
                         <div
@@ -55,7 +184,209 @@ const Books = () => {
                         </div>
                     ))}
                 </div>
-                <div className="books-jobb-kontener"></div>
+                <div className="books-jobb-kontener">
+                    <div className="jobb-fix">
+                        <h4>Árkategória (Ft)</h4>
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <label htmlFor="nincs">nincs</label>
+                                    </td>
+                                    <td>
+                                        <input
+                                            type="radio"
+                                            name="arkategoria"
+                                            id="nincs"
+                                            value="nincs"
+                                            onChange={(e) =>
+                                                setArak(e.target.value)
+                                            }
+                                        />
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <label htmlFor="0-1000">0-1000</label>
+                                    </td>
+                                    <td>
+                                        <input
+                                            type="radio"
+                                            name="arkategoria"
+                                            id="0-1000"
+                                            value="0-1000"
+                                            onChange={(e) =>
+                                                setArak(e.target.value)
+                                            }
+                                        />
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <label htmlFor="1000-2000">
+                                            1000-2000
+                                        </label>
+                                    </td>
+                                    <td>
+                                        <input
+                                            type="radio"
+                                            name="arkategoria"
+                                            id="1000-2000"
+                                            value="1000-2000"
+                                            onChange={(e) =>
+                                                setArak(e.target.value)
+                                            }
+                                        />
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <label htmlFor="2000-3000">
+                                            2000-3000
+                                        </label>
+                                    </td>
+                                    <td>
+                                        <input
+                                            type="radio"
+                                            name="arkategoria"
+                                            id="2000-3000"
+                                            value="2000-3000"
+                                            onChange={(e) =>
+                                                setArak(e.target.value)
+                                            }
+                                        />
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <label htmlFor="3000-4000">
+                                            3000-4000
+                                        </label>
+                                    </td>
+                                    <td>
+                                        <input
+                                            type="radio"
+                                            name="arkategoria"
+                                            id="3000-4000"
+                                            value="3000-4000"
+                                            onChange={(e) =>
+                                                setArak(e.target.value)
+                                            }
+                                        />
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <label htmlFor="4000-5000">
+                                            4000-5000
+                                        </label>
+                                    </td>
+                                    <td>
+                                        <input
+                                            type="radio"
+                                            name="arkategoria"
+                                            id="4000-5000"
+                                            value="4000-5000"
+                                            onChange={(e) =>
+                                                setArak(e.target.value)
+                                            }
+                                        />
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <label htmlFor="5000-6000">
+                                            5000-6000
+                                        </label>
+                                    </td>
+                                    <td>
+                                        <input
+                                            type="radio"
+                                            name="arkategoria"
+                                            id="5000-6000"
+                                            value="5000-6000"
+                                            onChange={(e) =>
+                                                setArak(e.target.value)
+                                            }
+                                        />
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <label htmlFor="6000-7000">
+                                            6000-7000
+                                        </label>
+                                    </td>
+                                    <td>
+                                        <input
+                                            type="radio"
+                                            name="arkategoria"
+                                            id="6000-7000"
+                                            value="6000-7000"
+                                            onChange={(e) =>
+                                                setArak(e.target.value)
+                                            }
+                                        />
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <label htmlFor="7000-8000">
+                                            7000-8000
+                                        </label>
+                                    </td>
+                                    <td>
+                                        <input
+                                            type="radio"
+                                            name="arkategoria"
+                                            id="7000-8000"
+                                            value="7000-8000"
+                                            onChange={(e) =>
+                                                setArak(e.target.value)
+                                            }
+                                        />
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <label htmlFor="8000-9000">
+                                            8000-9000
+                                        </label>
+                                    </td>
+                                    <td>
+                                        <input
+                                            type="radio"
+                                            name="arkategoria"
+                                            id="8000-9000"
+                                            value="8000-9000"
+                                            onChange={(e) =>
+                                                setArak(e.target.value)
+                                            }
+                                        />
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <label htmlFor="9000-10000">
+                                            9000-10000
+                                        </label>
+                                    </td>
+                                    <td>
+                                        <input
+                                            type="radio"
+                                            name="arkategoria"
+                                            id="9000-10000"
+                                            value="9000-10000"
+                                            onChange={(e) =>
+                                                setArak(e.target.value)
+                                            }
+                                        />
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
             ;
         </>
---
+++ md/feladatok_04.md
@@ -0,0 +1,12 @@
+# Backend (Server Side)
+
+-   [ ] Lépjünk be a **backend** mappába (Git Bash): `cd backend`
+-   [ ] Adjunk még néhány elemet az adatbázisba. Ehhez módosítsuk az **ujKonyv.ejs** és **konyvModosit.ejs** állományokat.
+-   [ ] Eredmény: **images/kep_071.png**
+-   [ ] Eredmény: **images/kep_072.png**
+
+# Frontend (Client Side)
+
+-   [ ] Lépjünk be a **frontend** mappába (Git Bash): `cd frontend`
+-   [ ] Módosítsuk a **Books.jsx** állományt: lásd állomány
+-   [ ] Módosítsuk a **Books.css** állományt: **images/kep_073.png**
